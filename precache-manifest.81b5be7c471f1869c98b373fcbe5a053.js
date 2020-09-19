f0d08340e88d119ae7c4ace55d590357fa2be91e
Quoc <quoc.lieu.95@gmail.com>
2020-09-19 20:49:14 +1000

    Updates

---
+++ precache-manifest.81b5be7c471f1869c98b373fcbe5a053.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edd92550a401ef6bc24ef4497ba80bef",
+    "url": "/photo-editor/index.html"
+  },
+  {
+    "revision": "9e8611997305f3f23aca",
+    "url": "/photo-editor/static/css/main.a68d7d2c.chunk.css"
+  },
+  {
+    "revision": "f09f8ff8eaaff7de4bde",
+    "url": "/photo-editor/static/js/2.a9dd16d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/photo-editor/static/js/2.a9dd16d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e8611997305f3f23aca",
+    "url": "/photo-editor/static/js/main.2cf031d6.chunk.js"
+  },
+  {
+    "revision": "5402a5877149a0224d4b",
+    "url": "/photo-editor/static/js/runtime-main.559063f6.js"
+  }
+]);
